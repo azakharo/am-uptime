4fee292a5d025e4ff4122b248e3c4c283e1f3678
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-01-17 17:59:26 +0300

    impl-ed pinging.

--- server/monitor.js
+++ server/monitor.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var ping = require('ping');
+
 exports.monitor = function() {
-  console.log("monitor");
+  var host = '10.10.2.163';
+  ping.sys.probe(host, function(isAlive){
+    var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+    console.log(msg);
+  });
 };

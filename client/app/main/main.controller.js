3c477c5685ca817c94611c5c23c1a474a17fad2c
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-01-19 18:54:24 +0300

    Impl-ed ui auto update.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('projectsApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $interval) {
 
     $scope.appVer = APP_VERSION;
     getDetectorsStatus();
+
+    // Auto-update
+    var statusUpd = $interval(getDetectorsStatus, 5000);
+    $scope.$on('$destroy', function () {
+      $interval.cancel(statusUpd);
+    });
 
     function getDetectorsStatus() {
       $http.get('/api/detectors').success(function(detectors) {

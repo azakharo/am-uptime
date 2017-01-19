7576b7cf8dcf4e25c07fc87ee54701698e55e597
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-01-19 18:27:27 +0300

    Disp detector stat including colors.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,9 +3,13 @@
 angular.module('projectsApp')
   .controller('MainCtrl', function ($scope, $http) {
 
-    $http.get('/api/detectors').success(function(detectors) {
-      log(detectors);
-    });
-
     $scope.appVer = APP_VERSION;
+    getDetectorsStatus();
+
+    function getDetectorsStatus() {
+      $http.get('/api/detectors').success(function(detectors) {
+        $scope.detectors = detectors;
+      });
+    }
+
   });
